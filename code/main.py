8b1b886ae03ed242ff3630a54768ba8bc82b2478
Benjamin <benjaminpalko@hotmail.com>
2019-03-17 16:20:13 -0400

    Initial commit

---
+++ code/main.py
@@ -0,0 +1,18 @@
+import simpy
+
+
+def component(env):
+    pass
+
+
+def product(env):
+    pass
+
+
+def main():
+    pass
+
+
+#   Checks if this is the main execution script in the program
+if __name__ == "__main__":
+    main()
