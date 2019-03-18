1a8698309e7cad9f633f6ed87fae198cb59bca2d
Benjamin <benjaminpalko@hotmail.com>
2019-03-17 23:04:57 -0400

    Auto stash before merge of "master" and "SYSC-4005-PROJECT/master"

--- code/main.py
+++ code/main.py
@@ -1,24 +1,26 @@
 import simpy
 from simpy.resources import container
 import random
-# import model
+import model
 
 
 class Inspector1(object):
+
     def __init__(self, env):
         self.env = env
         self.action = env.process(self.run())
 
     def run(self):
-        print('Inspector 2 starting')
+        print('Inspector 1 starting')
         while True:
-            service_time = 5  # <--Generate duration here
+            service_time = model.inspect1_comp1  # <--Generate duration here
             yield self.env.process(self.component_1(service_time))
 
     def component_1(self, duration):
         yield self.env.timeout(duration)
-        wait = False
+        wait = True
         while wait:
+            wait = False
             if workstation_1.component_container.level < 2:
                 workstation_1.component_container.put()
                 print('Added component 1 to workstation 1')
@@ -33,6 +35,7 @@ class Inspector1(object):
 
 
 class Inspector2(object):
+
     def __init__(self, env):
         self.env = env
         self.action = env.process(self.run())
@@ -41,22 +44,22 @@ class Inspector2(object):
         print('Inspector 2 starting')
         while True:
             if bool(random.getrandbits(1)):  # Randomly decides which component to make
-                service_time = 5  # <--Generate duration here
+                service_time = model.inspect2_comp2()  # <--Generate duration here
                 yield self.env.process(self.component_2(service_time))
             else:
-                service_time = 5  # <--Generate duration here
+                service_time = model.inspect2_comp3()  # <--Generate duration here
                 yield self.env.process(self.component_3(service_time))
 
     def component_2(self, duration):
         yield self.env.timeout(duration)
-        while workstation_2.component_container_2.level == 2:
+        while workstation_2.component_container_2.level >= 2:
             pass  # Blocks inspector while container is full
         workstation_2.component_container_2.put()
         print('Added component 2 to workstation 2')
 
     def component_3(self, duration):
         yield self.env.timeout(duration)
-        while workstation_3.component_container_3.level == 2:
+        while workstation_3.component_container_3.level >= 2:
             pass  # Blocks inspector while container is full
         workstation_3.component_container_3.put()
         print('Added component 3 to workstation 3')
@@ -75,6 +78,8 @@ class Workstation1(object):
             # Waits until there are components available to use
             if self.component_container.level > 0:
                 self.component_container.get()
+                duration = model.workstation1()  # <--Generate duration here
+                self.assemble_1(duration)
             else:
                 pass
 
@@ -92,13 +97,13 @@ class Workstation2(object):
         self.action = env.process(self.run())
 
     def run(self):
-        print('Workstation 1 starting')
+        print('Workstation 2 starting')
         while True:
             # Waits until there are components available to use
             if self.component_container_1.level > 0 and self.component_container_2.level > 0:
-                self.component_container_1.get(1)
-                self.component_container_2.get(1)
-                service_time = 5  # <--Generate duration here
+                self.component_container_1.get()
+                self.component_container_2.get()
+                service_time = model.workstation2()  # <--Generate duration here
                 self.assemble_2(service_time)
             else:
                 pass
@@ -117,13 +122,13 @@ class Workstation3(object):
         self.action = env.process(self.run())
 
     def run(self):
-        print('Workstation 1 starting')
+        print('Workstation 3 starting')
         while True:
             # Waits until there are components available to use
             if self.component_container_1.level > 0 and self.component_container_3.level > 0:
                 self.component_container_1.get()
                 self.component_container_3.get()
-                service_time = 5  # <--Generate duration here
+                service_time = model.workstation3()  # <--Generate duration here
                 self.assemble_3(service_time)
             else:
                 pass
@@ -134,7 +139,8 @@ class Workstation3(object):
 
 
 #   Checks if this is the main execution script in the program
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     print('Creating simulation environment')
     main_env = simpy.Environment()
     # Class instantiations
@@ -145,3 +151,5 @@ if __name__ == "__main__":
     workstation_3 = Workstation3(main_env)
     # Run simulation
     main_env.run(until=60)  # 'until' is simulation duration
+
+
