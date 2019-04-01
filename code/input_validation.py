c883352a4e529a332bc703f37db5e92a847bcec5
Unknown <thomas7carriere@gmail.com>
2019-04-01 16:28:50 -0400

    added validation for 30000 samples

--- code/input_validation.py
+++ code/input_validation.py
@@ -1,7 +1,7 @@
 import model
 
 
-def validate_inspect1_comp1():
+def validate_inspect1_comp1(n):
     datalist = open('servinsp1.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -9,15 +9,15 @@ def validate_inspect1_comp1():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0,1000):
+    for x in range(0, n):
         random_mean += model.inspect1_comp1()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Inspector 1 Component 1')
     print_mean(actual_mean, random_mean)
 
 
-def validate_inspect2_comp2():
+def validate_inspect2_comp2(n):
     datalist = open('servinsp22.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -25,15 +25,15 @@ def validate_inspect2_comp2():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0, 1000):
+    for x in range(0, n):
         random_mean += model.inspect2_comp2()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Inspector 2 Component 2')
     print_mean(actual_mean, random_mean)
 
 
-def validate_inspect2_comp3():
+def validate_inspect2_comp3(n):
     datalist = open('servinsp23.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -41,15 +41,15 @@ def validate_inspect2_comp3():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0, 1000):
+    for x in range(0, n):
         random_mean += model.inspect2_comp3()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Inspector 2 Component 3')
     print_mean(actual_mean, random_mean)
 
 
-def validate_workstation1():
+def validate_workstation1(n):
     datalist = open('ws1.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -57,15 +57,15 @@ def validate_workstation1():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0, 1000):
+    for x in range(0, n):
         random_mean += model.workstation1()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Workstation 1')
     print_mean(actual_mean, random_mean)
 
 
-def validate_workstation2():
+def validate_workstation2(n):
     datalist = open('ws2.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -73,15 +73,15 @@ def validate_workstation2():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0, 1000):
+    for x in range(0, n):
         random_mean += model.workstation2()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Workstation 2')
     print_mean(actual_mean, random_mean)
 
 
-def validate_workstation3():
+def validate_workstation3(n):
     datalist = open('ws3.dat').read().splitlines()
     actual_mean = 0
     for x in range(0, 300):
@@ -89,9 +89,9 @@ def validate_workstation3():
     actual_mean = actual_mean / 300
 
     random_mean = 0
-    for x in range(0, 1000):
+    for x in range(0, n):
         random_mean += model.workstation3()
-    random_mean = random_mean / 1000
+    random_mean = random_mean / n
 
     print('Workstation 3')
     print_mean(actual_mean, random_mean)
@@ -104,9 +104,9 @@ def print_mean(actual_mean, random_mean):
 
 
 if __name__ == '__main__':
-    validate_inspect1_comp1()
-    validate_inspect2_comp2()
-    validate_inspect2_comp3()
-    validate_workstation1()
-    validate_workstation2()
-    validate_workstation3()
+    validate_inspect1_comp1(30000)
+    validate_inspect2_comp2(30000)
+    validate_inspect2_comp3(30000)
+    validate_workstation1(30000)
+    validate_workstation2(30000)
+    validate_workstation3(30000)
